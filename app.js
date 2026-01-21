9c91b885b0d8ba2e71f3351e410a01299d441abf
Pranav Kamat <pranavkamat777@gmail.com>
2026-01-22 00:45:03 +0530

    new changes in mongoose conncetion

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-console.log("MONGO_URI:", process.env.MONGO_URI);
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -22,16 +19,35 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Atlas Connected");
+    console.log("DB Name:", mongoose.connection.name);
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error("MongoDB connection failed:", err);
+  });
+
+  mongoose.connection.on("error", err => {
+  console.error("Mongoose error:", err);
+});
+
+
 // --------------------
 // DATABASE CONNECTION
 // --------------------
-async function main() {
-  await mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Atlas Connected"))
-  .catch(err => console.error(err));
+// async function main() {
+//   await mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Atlas Connected"))
+//   .catch(err => console.error(err));
 
-}
-main().catch(err => console.log(err));
+// }
+// main().catch(err => console.log(err));
 
 // --------------------
 // ROUTES
@@ -172,6 +188,6 @@ app.use((err, req, res, next) => {
 // --------------------
 // SERVER
 // --------------------
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
